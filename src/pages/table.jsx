972470d30be685bd65820ee4055be6c208c54c2c
bruno10-0 <brunobrodon75@gmail.com>
2023-11-25 16:06:36 -0300

    quitando una propiedad tailwind a unas etiquetas

--- src/pages/table.jsx
+++ src/pages/table.jsx
@@ -52,7 +52,7 @@ export const Table = () => {
           )}
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-transparent">
             {idArray.length > 0 && (
-              <p className="mt-1 animate-ping" style={{ userSelect: 'none' }}>
+              <p className="mt-1" style={{ userSelect: 'none' }}>
                 {idArray.length}
               </p>
             )}
